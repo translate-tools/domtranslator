01324662d081050b81064e77ffc370c1cadc02d5
Irina Katsyuta <katsyuta@proton.me>
2025-04-27 00:24:39 +0200

    test: improve test

--- src/__tests__/DomNodeTranslator.test.ts
+++ src/__tests__/DomNodeTranslator.test.ts
@@ -1,148 +1,203 @@
 import { DOMTranslator } from '../DOMTranslator';
-import { visitWholeTree } from '../utils/visitWholeTree';
 import { awaitTranslation, containsRegex, TRANSLATION_SYMBOL, translator } from './utils';
 
-describe('DomNodeTranslator base usage', () => {
-	let domNodeTranslator: DOMTranslator;
-	let div: Element;
+beforeEach(() => {
+	document.body.innerHTML = '';
+});
 
-	beforeEach(() => {
-		div = document.createElement('div');
-		div.innerHTML = 'Hello world!';
-
-		const isTranslatableNode = (node: Node) => node instanceof Text;
-
-		domNodeTranslator = new DOMTranslator({
-			isTranslatableNode,
-			translateCallback: translator,
-		});
+test('Translate and restore original element text', async () => {
+	const domTranslator = new DOMTranslator({
+		isTranslatableNode: Boolean,
+		translateCallback: translator,
 	});
 
-	test('correct translate element', async () => {
-		const div = document.createElement('div');
-		const originalText = 'Hello world!';
-		div.innerHTML = originalText;
+	const originElementText = 'Hello world!';
+	const div = document.createElement('div');
+	div.innerHTML = originElementText;
 
-		domNodeTranslator.translateNode(div.childNodes[0]);
+	domTranslator.translateNode(div.childNodes[0]);
+	await awaitTranslation();
+
+	expect(div.innerHTML).toMatch(containsRegex(TRANSLATION_SYMBOL));
+
+	// disable translation
+	domTranslator.restoreNode(div.childNodes[0]);
+	expect(div.innerHTML).not.toMatch(containsRegex(TRANSLATION_SYMBOL));
+	expect(div.innerHTML).toMatch(originElementText);
+});
+
+test('Get original node text', async () => {
+	const domTranslator = new DOMTranslator({
+		isTranslatableNode: Boolean,
+		translateCallback: translator,
+	});
+
+	const originElementText = 'Hello world!';
+	const div = document.createElement('div');
+	div.innerHTML = originElementText;
+
+	// translate
+	domTranslator.translateNode(div.childNodes[0]);
+	await awaitTranslation();
+
+	expect(domTranslator.getOriginalNodeText(div.childNodes[0])).toEqual(
+		originElementText,
+	);
+});
+
+test('Not translate empty element', async () => {
+	const domTranslator = new DOMTranslator({
+		isTranslatableNode: Boolean,
+		translateCallback: translator,
+	});
+
+	const div = document.createElement('div');
+	div.innerHTML = ' ';
+
+	// translate
+	domTranslator.translateNode(div.childNodes[0]);
+	await awaitTranslation();
+
+	expect(div.childNodes[0].textContent).not.toMatch(containsRegex(TRANSLATION_SYMBOL));
+});
+
+test('Checks existing element in storage', async () => {
+	const domTranslator = new DOMTranslator({
+		isTranslatableNode: Boolean,
+		translateCallback: translator,
+	});
+	const div = document.createElement('div');
+	div.innerHTML = 'Hello world!';
+
+	domTranslator.translateNode(div.childNodes[0]);
+	await awaitTranslation();
+
+	expect(domTranslator.hasNode(div.childNodes[0])).toBe(true);
+
+	//delete element
+	domTranslator.restoreNode(div.childNodes[0]);
+	expect(domTranslator.hasNode(div.childNodes[0])).toBe(false);
+});
+
+test('Update translation for element ', async () => {
+	const domTranslator = new DOMTranslator({
+		isTranslatableNode: Boolean,
+		translateCallback: translator,
+	});
+	const div = document.createElement('div');
+	const originalText = 'Hello world!';
+	div.innerHTML = originalText;
+
+	// spy on the updateNode method
+	const updateNodesSpy = vi.spyOn(domTranslator as DOMTranslator, 'updateNode');
+
+	// translate element
+	domTranslator.translateNode(div.childNodes[0]);
+	await awaitTranslation();
+
+	// update element
+	const newText = 'Goodbye world!';
+	div.innerHTML = newText;
+	domTranslator.translateNode(div.childNodes[0]);
+	await awaitTranslation();
+
+	domTranslator.updateNode(div.childNodes[0]);
+	await awaitTranslation();
+
+	// correct text translation
+	expect(div.innerHTML).toMatch(containsRegex(TRANSLATION_SYMBOL));
+	expect(div.innerHTML).toMatch(newText);
+
+	// update calls one time
+	expect(updateNodesSpy).toBeCalledTimes(1);
+	expect(updateNodesSpy.mock.calls[0][0]).toMatchObject(
+		containsRegex(TRANSLATION_SYMBOL),
+	);
+});
+
+describe('Restore node', () => {
+	// mock for to translate the entire element tree
+	const handleElementTree = (node: Node, callback: (node: Node) => void) => {
+		if (node instanceof Element) {
+			vi.fn((root: Element, callback: (n: Node) => void) => {
+				const handel = (n: Node) => {
+					callback(n);
+					if (n instanceof Element) {
+						Array.from(n.childNodes).forEach(handel);
+						Array.from(n.attributes).forEach(callback);
+					}
+				};
+				handel(root);
+			})(node, (node) => {
+				callback(node);
+			});
+		}
+	};
+
+	test('Restore the text element after a few translations', async () => {
+		const domTranslator = new DOMTranslator({
+			isTranslatableNode: Boolean,
+			translateCallback: translator,
+		});
+		const div = document.createElement('div');
+		div.innerHTML = 'Hello world!';
+		domTranslator.translateNode(div.childNodes[0]);
 		await awaitTranslation();
 
-		expect(div.innerHTML).toMatch(containsRegex(TRANSLATION_SYMBOL));
+		// update text
+		const newText = 'Hello world 1234!';
+		div.innerHTML = newText;
+		domTranslator.translateNode(div.childNodes[0]);
+		await awaitTranslation();
 
-		// disable translation
-		domNodeTranslator.restoreNode(div.childNodes[0]);
+		// restore
+		domTranslator.restoreNode(div.childNodes[0]);
+		expect(div.innerHTML).toMatch(newText);
 		expect(div.innerHTML).not.toMatch(containsRegex(TRANSLATION_SYMBOL));
 	});
 
-	test('getNodeData returns the original text', async () => {
-		const originalText = 'Hello world!';
+	test('Restore translations from all nested nodes in the element', async () => {
+		const domTranslator = new DOMTranslator({
+			isTranslatableNode: Boolean,
+			translateCallback: translator,
+		});
+		const parentDiv = document.createElement('div');
+		parentDiv.innerHTML = 'Hello world!';
+		const childDiv = document.createElement('div');
+		childDiv.innerHTML = 'Hello world too!';
+		parentDiv.append(childDiv);
 
-		// translate
-		domNodeTranslator.translateNode(div.childNodes[0]);
+		handleElementTree(parentDiv, domTranslator.translateNode);
 		await awaitTranslation();
 
-		expect(domNodeTranslator.getOriginalNodeText(div.childNodes[0])).toEqual(
-			expect.objectContaining({
-				originalText: originalText,
-			}),
-		);
+		domTranslator.restoreNode(parentDiv);
+
+		// child node and target has not translated text
+		expect(parentDiv.innerHTML).not.toMatch(containsRegex(TRANSLATION_SYMBOL));
+		expect(childDiv.innerHTML).not.toMatch(containsRegex(TRANSLATION_SYMBOL));
 	});
 
-	test('not translate empty element', async () => {
-		div.innerHTML = ' ';
+	test('Delete translation only from target element', async () => {
+		const domTranslator = new DOMTranslator({
+			isTranslatableNode: Boolean,
+			translateCallback: translator,
+		});
+		const parentDiv = document.createElement('div');
+		parentDiv.innerHTML = 'Hello world!';
+		const childDiv = document.createElement('div');
+		childDiv.innerHTML = 'Hello world too!';
+		parentDiv.append(childDiv);
 
-		// translate
-		domNodeTranslator.translateNode(div.childNodes[0]);
+		handleElementTree(parentDiv, domTranslator.translateNode);
 		await awaitTranslation();
 
-		expect(div.childNodes[0].textContent).not.toMatch(
+		domTranslator.restoreNode(parentDiv.childNodes[0], true);
+
+		//target element has not translation
+		expect(parentDiv.childNodes[0].textContent).not.toMatch(
 			containsRegex(TRANSLATION_SYMBOL),
 		);
-	});
-
-	test('isNodeStorageHas returns correct result', async () => {
-		domNodeTranslator.translateNode(div.childNodes[0]);
-		await awaitTranslation();
-
-		expect(domNodeTranslator.hasNode(div.childNodes[0])).toBe(true);
-
-		//delete element
-		domNodeTranslator.restoreNode(div.childNodes[0]);
-		expect(domNodeTranslator.hasNode(div.childNodes[0])).toBe(false);
-	});
-
-	describe('DeleteNode', () => {
-		let parentDiv: Element;
-		let childDiv: Element;
-
-		const handleElementTree = (node: Node, callback: (node: Node) => void) => {
-			if (node instanceof Element) {
-				visitWholeTree(node, (node) => {
-					callback(node);
-				});
-			}
-		};
-
-		beforeEach(() => {
-			parentDiv = document.createElement('div');
-			parentDiv.innerHTML = 'Hello world!';
-
-			childDiv = document.createElement('div');
-			childDiv.innerHTML = 'Hello world too!';
-			parentDiv.append(childDiv);
-		});
-
-		test('delete translations from all nodes in the tree', async () => {
-			handleElementTree(parentDiv, domNodeTranslator.translateNode);
-			await awaitTranslation();
-
-			domNodeTranslator.restoreNode(parentDiv);
-
-			// child node and target has not translated text
-			expect(parentDiv.innerHTML).not.toMatch(containsRegex(TRANSLATION_SYMBOL));
-			expect(childDiv.innerHTML).not.toMatch(containsRegex(TRANSLATION_SYMBOL));
-		});
-
-		test('delete translation from the selected node', async () => {
-			handleElementTree(parentDiv, domNodeTranslator.translateNode);
-			await awaitTranslation();
-
-			domNodeTranslator.restoreNode(parentDiv.childNodes[0], true);
-
-			//target element has not translation
-			expect(parentDiv.childNodes[0].textContent).not.toMatch(
-				containsRegex(TRANSLATION_SYMBOL),
-			);
-			// child element still has translation
-			expect(childDiv.innerHTML).toMatch(containsRegex(TRANSLATION_SYMBOL));
-		});
-	});
-
-	test('updateNode should be call ones', async () => {
-		const div = document.createElement('div');
-		const originalText = 'Hello world!';
-		div.innerHTML = originalText;
-
-		// spy on the updateNode method
-		const updateNodesSpy = vi.spyOn(domNodeTranslator as DOMTranslator, 'updateNode');
-
-		// translate element
-		domNodeTranslator.translateNode(div.childNodes[0]);
-		await awaitTranslation();
-
-		// update element
-		const newText = 'Goodbye world!';
-		div.innerHTML = newText;
-
-		domNodeTranslator.translateNode(div.childNodes[0]);
-		await awaitTranslation();
-
-		domNodeTranslator.updateNode(div.childNodes[0]);
-		await awaitTranslation();
-
-		expect(div.innerHTML).toMatch(containsRegex(TRANSLATION_SYMBOL));
-		expect(updateNodesSpy).toBeCalledTimes(1);
-		expect(updateNodesSpy.mock.calls[0][0]).toMatchObject(
-			containsRegex(TRANSLATION_SYMBOL),
-		);
+		// child element still has translation
+		expect(childDiv.innerHTML).toMatch(containsRegex(TRANSLATION_SYMBOL));
 	});
 });
