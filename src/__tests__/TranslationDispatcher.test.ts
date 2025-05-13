6620af5ba02282512cd732abba4bce8c55135902
Irina Katsyuta <katsyuta@proton.me>
2025-05-13 18:15:56 +0200

    test: add test case

--- src/__tests__/TranslationDispatcher.test.ts
+++ src/__tests__/TranslationDispatcher.test.ts
@@ -1,6 +1,7 @@
 import { DOMNodesTranslator } from '../DOMNodesTranslator';
 import { IntersectionObserverWithFilter } from '../IntersectionObserverWithFilter';
 import { TranslationDispatcher } from '../TranslationDispatcher';
+import { configureTranslatableNodePredicate } from '../utils/nodes';
 import { awaitTranslation, containsRegex, TRANSLATION_SYMBOL, translator } from './utils';
 
 require('intersection-observer');
@@ -88,4 +89,37 @@ test('Translates and restores the original text content of an element and its ch
 	await awaitTranslation();
 	expect(div.childNodes[0].textContent).toBe(text);
 	expect(div1.childNodes[0].textContent).toBe(text1);
+});
+
+test('IntersectionObserver does not translate ignored node inside target', async () => {
+	const isTranslatableNode = configureTranslatableNodePredicate({
+		ignoredSelectors: ['comment'],
+	});
+
+	const domNodesTranslator = new DOMNodesTranslator(translator);
+	const translationDispatcher = new TranslationDispatcher({
+		filter: isTranslatableNode,
+		nodeTranslator: domNodesTranslator,
+		lazyTranslator: new IntersectionObserverWithFilter({
+			filter: isTranslatableNode,
+			onIntersected: domNodesTranslator.translateNode,
+		}),
+	});
+
+	const div = document.createElement('div');
+	div.textContent = 'I`m block i have four corners';
+	// Comment element should not be translated
+	const comment = document.createComment('I`m comment node, not translate me please');
+	div.appendChild(comment);
+	document.body.appendChild(div);
+
+	// IntersectionObserverWithFilter receives a div element containing a comment that should not be translated
+	translationDispatcher.translateNode(div);
+	await awaitTranslation();
+
+	// call IntersectionObserverWithFilter with element
+	expect(lazyTranslatorSpy.mock.calls).toEqual([[div]]);
+	expect(div.textContent).toMatch(containsRegex(TRANSLATION_SYMBOL));
+	// comment not translated
+	expect(div.childNodes[1].textContent).not.toMatch(containsRegex(TRANSLATION_SYMBOL));
 });
