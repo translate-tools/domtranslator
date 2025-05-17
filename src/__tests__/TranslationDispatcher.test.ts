41e98da140157a8b47e03b934a5cfeeee1db6874
Irina Katsyuta <katsyuta@proton.me>
2025-05-17 21:48:24 +0200

    test: change test case

--- src/__tests__/TranslationDispatcher.test.ts
+++ src/__tests__/TranslationDispatcher.test.ts
@@ -15,7 +15,6 @@ const isTranslatableNode = () => true;
 
 test('Translate node that is not suitable for delayed translation', async () => {
 	const domNodesTranslator = new DOMNodesTranslator(translator);
-
 	const translationDispatcher = new TranslationDispatcher({
 		filter: isTranslatableNode,
 		nodeTranslator: domNodesTranslator,
@@ -35,32 +34,28 @@ test('Translate node that is not suitable for delayed translation', async () => 
 	expect(option.textContent).toMatch(containsRegex(TRANSLATION_SYMBOL));
 });
 
-test('Translate node from shadowDom ', async () => {
+test('Translate node not attached to DOM', async () => {
 	const domNodesTranslator = new DOMNodesTranslator(translator);
-
 	const translationDispatcher = new TranslationDispatcher({
 		filter: isTranslatableNode,
 		nodeTranslator: domNodesTranslator,
 		nodeIntersectionObserver: new NodesIntersectionObserver(),
 	});
 
-	const host = document.createElement('div');
-	document.body.appendChild(host);
-	const shadowRoot = host.attachShadow({ mode: 'open' });
-
 	// this node not attached to DOM, but should be translated
-	const div = document.createElement('div');
-	const text = 'I`m from shadow';
-	div.textContent = text;
-	shadowRoot.appendChild(div);
+	const head = document.createElement('head');
+	const title = document.createElement('title');
+	const text = 'Title can contain only text';
+	title.textContent = text;
+	head.appendChild(title);
 
-	translationDispatcher.translateNode(host);
+	translationDispatcher.translateNode(head);
 	await awaitTranslation();
-	expect(div.textContent).toMatch(containsRegex(TRANSLATION_SYMBOL));
+	expect(title.textContent).toMatch(containsRegex(TRANSLATION_SYMBOL));
 
 	// restore
-	translationDispatcher.restoreNode(host);
-	expect(div.textContent).toBe(text);
+	translationDispatcher.restoreNode(head);
+	expect(title.textContent).toBe(text);
 });
 
 test('Translates and restores the element and its child elements', async () => {
