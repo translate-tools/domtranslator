92407a4f8941592f33f8ffa3c6258fe31564c5ff
Robert Vitonsky <robert@vitonsky.net>
2025-07-02 17:30:01 +0200

    docs: explain scheduler purpose

--- src/IntersectionScheduler.ts
+++ src/IntersectionScheduler.ts
@@ -1,6 +1,10 @@
 import { DOMTranslationScheduler } from './types';
 import { NodesIntersectionObserver } from './utils/NodesIntersectionObserver';
 
+/**
+ * Scheduler for DOM translation that trigger callbacks only for visible nodes.
+ * If node is not visible, callback will be triggered once node will intersect viewport.
+ */
 export class IntersectionScheduler implements DOMTranslationScheduler {
 	private readonly intersectionObserver;
 	constructor(intersectionConfig?: IntersectionObserverInit) {
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -11,7 +11,23 @@ export interface StateStorage<S> {
 	getState(node: Node): S | null;
 }
 
+/**
+ * Scheduler that define when node will be translated.
+ * 
+ * For example, node translation may be delayed if node is not visible,
+ * or if pool of requests is full, or to score nodes priority in tree,
+ * and translate it in order of importance.
+ */
 export interface DOMTranslationScheduler {
+	/**
+	 * Add node to scheduler
+	 * 
+	 * Callback will be called when scheduler decide to start translate a node
+	 */
 	add(node: Node, callback: (node: Node) => void): void;
+
+	/**
+	 * Remove node of scheduler
+	 */
 	remove(node: Node): void;
 }
