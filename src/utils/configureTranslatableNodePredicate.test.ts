77b1433cf069c1869d7f1cec868a022ec1b6b604
Robert Vitonsky <robert@vitonsky.net>
2025-07-04 21:40:37 +0200

    test: add cases for attributes

--- src/utils/configureTranslatableNodePredicate.test.ts
+++ src/utils/configureTranslatableNodePredicate.test.ts
@@ -4,8 +4,8 @@ beforeEach(() => {
 	vi.clearAllMocks();
 	document.body.innerHTML = `
 	<div class="container">
-		<div class="card">
-			<img src="#"/>
+		<div class="card" data-foo="1" data-bar="2">
+			<img src="#" alt="Image description" title="Image title"/>
 		</div>
 	</div>
 	`;
@@ -27,4 +27,41 @@ test('invalid selectors must be ignored', () => {
 	expect(filter(img)).toBe(false);
 
 	expect(filter(card.getAttributeNode('class') as Node)).toBe(false);
+});
+
+test('attributes on non-matched nodes is skipped', () => {
+	const container = document.querySelector('.container') as HTMLElement;
+	const card = document.querySelector('.card') as HTMLElement;
+	const img = document.querySelector('.card img') as HTMLElement;
+
+	const filter = configureTranslatableNodePredicate({
+		ignoredSelectors: ['.card'],
+	});
+
+	expect(filter(img.getAttributeNode('alt') as Node)).toBe(false);
+
+	expect(filter(container)).toBe(true);
+	expect(filter(card)).toBe(false);
+	expect(filter(img)).toBe(false);
+});
+
+test('only attributes in list is match', () => {
+	const container = document.querySelector('.container') as HTMLElement;
+	const card = document.querySelector('.card') as HTMLElement;
+	const img = document.querySelector('.card img') as HTMLElement;
+
+	const filter = configureTranslatableNodePredicate({
+		translatableAttributes: ['title', 'data-foo'],
+	});
+
+	expect(filter(img.getAttributeNode('title') as Node)).toBe(true);
+	expect(filter(img.getAttributeNode('alt') as Node)).toBe(false);
+	expect(filter(img.getAttributeNode('src') as Node)).toBe(false);
+
+	expect(filter(card.getAttributeNode('data-foo') as Node)).toBe(true);
+	expect(filter(card.getAttributeNode('data-bar') as Node)).toBe(false);
+
+	expect(filter(container)).toBe(true);
+	expect(filter(card)).toBe(true);
+	expect(filter(img)).toBe(true);
 });
